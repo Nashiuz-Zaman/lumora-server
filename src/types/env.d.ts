b16c44d14ff3a7e4777a7e06bc493aeced1c1c18
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2025-08-17 17:12:00 +0600

    Add process env and ssl commerce types

---
+++ src/types/env.d.ts
@@ -0,0 +1,28 @@
+// src/types/env.d.ts
+declare namespace NodeJS {
+  interface ProcessEnv {
+    PORT: string;
+    MONGO_URI: string;
+    SMTP_USER_EMAIL: string;
+    SMTP_APP_PASSWORD: string;
+    NODE_ENV: "development" | "production" | "test";
+
+    DEV_CLIENT_DOMAIN: string;
+    DEV_CLIENT_URL: string;
+    PROD_CLIENT_DOMAIN: string;
+    PROD_CLIENT_URL: string;
+
+    ACCESS_TOKEN_SECRET: string;
+    REFRESH_TOKEN_SECRET: string;
+    CART_TOKEN_SECRET: string;
+
+    API_VERSION: string;
+
+    SUPERADMIN_EMAIL: string;
+    SUPERADMIN_PASSWORD: string;
+
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_SECRET: string;
+    CLOUDINARY_API_KEY: string;
+  }
+}
