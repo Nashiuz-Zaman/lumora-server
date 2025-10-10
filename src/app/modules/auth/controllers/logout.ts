e6dcae0cdf9645ed73c40eb63073c4bc6b11fdfa
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2025-10-11 04:08:54 +0600

    Trial

--- src/app/modules/auth/controllers/logout.ts
+++ src/app/modules/auth/controllers/logout.ts
@@ -2,11 +2,30 @@
 import { RequestHandler } from "express";
 
 // utils
-import { catchAsync, cleanCookie, sendSuccess } from "@utils/index";
+import { catchAsync, sendSuccess } from "@utils/index";
 
 export const logout: RequestHandler = catchAsync(async (_, res) => {
-  cleanCookie(res, "Access_Token");
-  cleanCookie(res, "Refresh_Token");
+  // Clear Access_Token
+  res.clearCookie("Access_Token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+    domain:
+      process.env.NODE_ENV === "production"
+        ? "lumora-server.vercel.app"
+        : undefined,
+    path: "/",
+  });
+
+  // Clear Refresh_Token
+  res.clearCookie("Refresh_Token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    domain: "lumora-server.vercel.app",
+
+    path: "/",
+  });
 
   sendSuccess(res, { message: "Logged Out" });
 });
