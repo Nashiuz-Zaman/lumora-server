e73872778aa6148ece6d28f5910ccf77f89c7cdf
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2025-10-11 21:11:47 +0600

    Testing with sparticuz chromium and puppeteer core

--- src/utils/generatePdfBufferFromHtml.ts
+++ src/utils/generatePdfBufferFromHtml.ts
@@ -1,12 +1,24 @@
-import puppeteer from "puppeteer";
+import Chromium from "@sparticuz/chromium";
+import puppeteer from "puppeteer-core";
+
+const viewport = {
+  deviceScaleFactor: 1,
+  hasTouch: false,
+  height: 1080,
+  isLandscape: true,
+  isMobile: false,
+  width: 1920,
+};
 
 export const generatePdfBufferFromHtml = async (
   html: string
 ): Promise<Buffer> => {
-  // Launch Puppeteer (headless Chromium)
+  // Launch Puppeteer with serverless-compatible Chromium
   const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: "shell",
+    args: Chromium.args,
+    defaultViewport: viewport,
+    executablePath: await Chromium.executablePath(),
   });
 
   try {
