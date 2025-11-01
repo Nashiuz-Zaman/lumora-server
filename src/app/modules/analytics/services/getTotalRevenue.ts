b0fb7890b345aaa99fbc6fec642eadd65c84a7dc
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2025-11-02 04:03:28 +0600

    Changed the functionality of getTotalRevenue service of analytics module

--- src/app/modules/analytics/services/getTotalRevenue.ts
+++ src/app/modules/analytics/services/getTotalRevenue.ts
@@ -1,31 +1,27 @@
-import { OrderModel } from "@app/modules/order/order.model";
-import { OrderStatus } from "@app/modules/order/order.constants";
+import { PaymentType } from "@app/modules/payment/payment.constant";
+import { PaymentModel } from "@app/modules/payment/payment.model";
 import { throwInternalServerError } from "@utils/index";
 
 export const getTotalRevenue = async (): Promise<number> => {
-  const result = await OrderModel.aggregate([
-    {
-      $match: {
-        status: {
-          $nin: [
-            OrderStatus.Pending,
-            OrderStatus.Returned,
-            OrderStatus.Cancelled,
-          ],
-        },
-      },
-    },
+  const result = await PaymentModel.aggregate([
     {
       $group: {
         _id: null,
-        totalRevenue: { $sum: "$total" },
+        totalRevenue: {
+          $sum: {
+            $cond: [
+              { $eq: ["$type", PaymentType.payment] },
+              "$amount",
+              { $multiply: ["$amount", -1] }, // subtract refunds
+            ],
+          },
+        },
       },
     },
   ]);
 
   const totalRevenue = result[0]?.totalRevenue ?? 0;
-
   if (typeof totalRevenue !== "number") return throwInternalServerError();
-
+  
   return totalRevenue;
 };
